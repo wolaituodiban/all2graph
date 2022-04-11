9cfb143dad92530af4903113159cc3e47f619c3a
陈小天 <wolaituodiban@gmail.com>
2022-04-11 19:11:22 +0800

    add function: git commit will be autoly added to __version__

--- all2graph/version.py
+++ all2graph/version.py
@@ -1,1 +1,14 @@
+import subprocess
+import os
+
+
 __version__ = '0.4.0'
+old_dir = os.getcwd()
+os.chdir(os.path.dirname(os.path.dirname(__file__)))
+try:
+    __version__ = '.'.join([__version__, subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8')])
+except:
+    pass
+finally:
+    os.chdir(old_dir)
+
