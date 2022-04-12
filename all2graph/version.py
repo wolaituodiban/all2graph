34b88cc05360d68349085f259cd62943d7dde681
陈小天 <wolaituodiban@gmail.com>
2022-04-12 17:03:50 +0800

    secbone

--- all2graph/version.py
+++ all2graph/version.py
@@ -3,12 +3,10 @@ import os
 from .globals import __version__
 
 
-old_dir = os.getcwd()
-os.chdir(os.path.dirname(os.path.dirname(__file__)))
 try:
+    old_dir = os.getcwd()
+    os.chdir(os.path.dirname(os.path.dirname(__file__)))
     __version__ = '.'.join([__version__, subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8')])
+    os.chdir(old_dir)
 except:
     pass
-finally:
-    os.chdir(old_dir)
-
