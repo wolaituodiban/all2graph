30bff14d653179f650d53057f0e48cabf3412cb1
陈小天 <wolaituodiban@gmail.com>
2022-04-12 17:16:31 +0800

    fix bug: remove \n in version

--- all2graph/version.py
+++ all2graph/version.py
@@ -6,7 +6,7 @@ from .globals import __version__
 try:
     old_dir = os.getcwd()
     os.chdir(os.path.dirname(os.path.dirname(__file__)))
-    __version__ = '.'.join([__version__, subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8')])
+    __version__ = '.'.join([__version__, subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()])
     os.chdir(old_dir)
 except:
     pass
