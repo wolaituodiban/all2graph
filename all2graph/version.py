ae304faca01052750e6bde3f5ea84aed9d9ca357
陈小天 <wolaituodiban@gmail.com>
2022-05-19 12:11:42 +0800

    fix bug: import issue

--- all2graph/version.py
+++ all2graph/version.py
@@ -1,30 +1,17 @@
-import platform
 import os
 import subprocess
 from .globals import __version__
 
 
-def append_version():
-    return '.'.join(
-        [__version__, subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()])
-
-
-all2graph_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if 'linux' in platform.system().lower():
-    old_home = os.environ['HOME']
-    try:
-        os.environ['HOME'] = all2graph_root
-        __version__ = append_version()
-    except:
-        pass
-    finally:
-        os.environ['HOME'] = old_home
-else:
+try:
     old_dir = os.getcwd()
     try:
-        os.chdir(all2graph_root)
-        __version__ = append_version()
+        os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        __version__ = '.'.join(
+            [__version__, subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()])
     except:
         pass
     finally:
         os.chdir(old_dir)
+except:
+    pass
